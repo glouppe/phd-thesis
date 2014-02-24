9006265d5184db5904c4ee2c7f6ef8cf4e38ea9b
Gilles Louppe <g.louppe@gmail.com>
2014-02-24 09:23:47 +0100

    improve figures

--- scripts/ch3_split_ordered_invariant.py
+++ scripts/ch3_split_ordered_invariant.py
@@ -17,10 +17,38 @@ y[n_samples / 2:] = 1
 
 plt.plot([-3,3], [0,0], '-', color='k')
 
-X_ = np.sort(X)
+indices = np.argsort(X)
+X_ = X[indices]
 for i in range(len(X_) - 1):
     s = (X_[i]+X_[i+1]) / 2.0
-    plt.plot([s,s], [0.00001, -0.00001], ':', color=gray)
+    plt.plot([s,s], [0.00001, 0], ':', color=gray)
+
+y_ = y[indices]
+
+def gini(p):
+    p1 = 1.0 * np.sum(p) / len(p)
+    p0 = 1.0 - p1
+    return p0 * (1 - p0) + p1 * (1 - p1)
+
+s = []
+delta = []
+
+for i in range(1, 8):
+    i_t = gini(y)
+    i_t_L = gini(y[:i])
+    i_t_R = gini(y[i:])
+    p_L = 1.0 * i / n_samples
+    p_R = 1.0 - p_L
+
+    s.append((X_[i-1] + X_[i]) / 2.0)
+    delta.append(i_t - p_L * i_t_L - p_R * i_t_R)
+
+delta = np.array(delta)
+delta /= np.max(delta)
+delta *= 0.00001
+
+plt.plot(s, delta, "o-")
+
 
 plt.scatter(X[:n_samples / 2], np.zeros(n_samples / 2), color=blue)
 plt.scatter(X[n_samples / 2:], np.zeros(n_samples / 2), color=red)
@@ -30,20 +58,22 @@ s2 = X_[7]
 smid = (s1+s2) / 2.0
 
 #plt.plot([s2,s2], [0.00001, -0.00001], '-', color=gray)
-plt.text(s1, 0.000001, "$x_{i-1,j}$", fontsize=15, horizontalalignment='center')
-plt.text(s2, 0.000001, "$x_{i,j}$", fontsize=15, horizontalalignment='center')
+plt.text(s1, 0.00000095, "$x_{i-1,j}$", fontsize=15, horizontalalignment='center')
+plt.text(s2, 0.00000095, "$x_{i,j}$", fontsize=15, horizontalalignment='center')
 plt.text(smid, 0.000011, "$v^\prime_k$", fontsize=15, horizontalalignment='center')
-plt.text((smid+(-3)) / 2.0, -0.0000095, "${\cal L}^{v^\prime_k}_{t_L}$", fontsize=15, horizontalalignment='center')
-plt.text((smid+3) / 2.0, -0.0000095, "${\cal L}^{v^\prime_k}_{t_R}$", fontsize=15, horizontalalignment='center')
+plt.text((smid+(-3)) / 2.0, -0.000001, "${\cal L}^{v^\prime_k}_{t_L}$", fontsize=15, horizontalalignment='center')
+plt.text((smid+3) / 2.0, -0.000001, "${\cal L}^{v^\prime_k}_{t_R}$", fontsize=15, horizontalalignment='center')
 
 plt.annotate(
-    '', xy=(-3, -0.000011), xycoords = 'data',
-    xytext = (smid, -0.000011), textcoords = 'data',
+    '', xy=(-3, -0.0000015), xycoords = 'data',
+    xytext = (smid, -0.0000015), textcoords = 'data',
     arrowprops = {'arrowstyle':'<->'})
 plt.annotate(
-    '', xy=(smid, -0.000011), xycoords = 'data',
-    xytext = (3, -0.000011), textcoords = 'data',
+    '', xy=(smid, -0.0000015), xycoords = 'data',
+    xytext = (3, -0.0000015), textcoords = 'data',
     arrowprops = {'arrowstyle':'<->'})
+
+plt.annotate("$\Delta$", xy=(s[4], delta[4]), xycoords='data', xytext=(s[4]-0.5, delta[4]-0.000001), textcoords='data', arrowprops={'arrowstyle':'->'})
 
 plt.text(3, -0.000003, "$X_j$", fontsize=15)
 
