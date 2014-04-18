77f36be8a6308abc02183f79f1d29ff9b7d151fa
Gilles Louppe <g.louppe@gmail.com>
2014-04-18 20:15:02 +0200

    OpenCV wrapper

--- benchmarks/wrapper.py
+++ benchmarks/wrapper.py
@@ -4,11 +4,13 @@ import numpy as np
 from sklearn.base import BaseEstimator, ClassifierMixin
 from sklearn.externals import six
 
+
+# R randomForest ==============================================================
+
 from rpy2.robjects.numpy2ri import numpy2ri
 from rpy2.robjects.packages import importr
 from rpy2 import robjects as ro
 rf = importr('randomForest')
-
 
 class RRandomForestClassifier(BaseEstimator, ClassifierMixin):
     def __init__(self, n_estimators=10,
@@ -65,3 +67,73 @@ class RRandomForestClassifier(BaseEstimator, ClassifierMixin):
         pred = np.array(pred, dtype=np.int32) - 1
         return self.classes_[pred]
 
+
+# OpenCV Random Trees =========================================================
+
+import cv2
+
+class OpenCVRandomForestClassifier(BaseEstimator, ClassifierMixin):
+    def __init__(self, n_estimators=10,
+                       max_depth=None,
+                       min_samples_split=2,
+                       max_features="auto",
+                       random_state=None):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.max_features = max_features
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        # Check params
+        self.n_features_ = X.shape[1]
+
+        if isinstance(self.max_features, str):
+            if self.max_features == "auto":
+                max_features = max(1, int(np.sqrt(self.n_features_)))
+            elif self.max_features == "sqrt":
+                max_features = max(1, int(np.sqrt(self.n_features_)))
+            elif self.max_features == "log2":
+                max_features = max(1, int(np.log2(self.n_features_)))
+            else:
+                raise ValueError(
+                    'Invalid value for max_features. Allowed string '
+                    'values are "auto", "sqrt" or "log2".')
+        elif self.max_features is None:
+            max_features = self.n_features_
+        elif isinstance(self.max_features, (numbers.Integral, np.integer)):
+            max_features = self.max_features
+        else:  # float
+            max_features = int(self.max_features * self.n_features_)
+
+        params = {}
+        params["nactive_vars"] = max_features
+        params["max_num_of_trees_in_the_forest"] = self.n_estimators
+        params["min_sample_count"] = self.min_samples_split
+        params["calc_var_importance"] = False
+        params["max_depth"] = 10E9 if self.max_depth is None else self.max_depth
+        params["use_surrogates"] = False
+        params["termcrit_type"] = (cv2.TERM_CRITERIA_MAX_ITER, self.n_estimators, 1)
+
+        # Convert data
+        self.classes_ = np.unique(y)
+        y = np.searchsorted(self.classes_, y)
+        X = X.astype(np.float32)
+        y = y.astype(np.float32)
+
+        # Run
+        self.model_ = cv2.RandomTrees()
+        self.model_.train(X, cv2.TERM_CRITERIA_MAX_ITER, y, params=params)
+
+        return self
+
+    def predict(self, X):
+        X = X.astype(np.float32)
+        pred = np.zeros(len(X))
+
+        for i in len(X):
+            pred[i] = self.model_.predict(X[i])
+
+        pred = pred.astype(np.int32)
+
+        return self.classes_[pred]
