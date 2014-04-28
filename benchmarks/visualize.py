0bbaab36c63c0daffc32d563fb809aaa32f03715
Gilles Louppe <g.louppe@gmail.com>
2014-04-28 14:30:22 +0200

    5.4.1

--- benchmarks/visualize.py
+++ benchmarks/visualize.py
@@ -72,13 +72,18 @@ def plot_curve(all_data, x_label=None, y_label=None, width=0.2, curve=True, file
         x = [offset*t[0]+i*width for t in data]
         y = [np.mean(t[1]) for t in data]
 
+        if x_label == "n_estimators":
+            xticks = [t[0] for t in data if t[0] < 1000]
+            x = [offset*t[0]+i*width for t in data if t[0] < 1000]
+            y = [np.mean(t[1]) for t in data if t[0] < 1000]
+
+        if y_label == "MSE":
+            y = [-y_i for y_i in y]
+
         if curve:
             ax.plot(xticks, y, label=name, color=cmap_curve[i])
         else:
             ax.bar(x, y, width=width, label=name, color=cmap_curve[i])
-
-    if x_label is not None: ax.set_xlabel(x_label)
-    if y_label is not None: ax.set_ylabel(y_label)
 
     if curve:
         ax.set_xlim(xticks[0], xticks[-1])
@@ -87,8 +92,11 @@ def plot_curve(all_data, x_label=None, y_label=None, width=0.2, curve=True, file
         ax.set_xticks(x)
         ax.set_xticklabels(xticks)
 
+    if x_label is not None: ax.set_xlabel(x_label)
+    if y_label is not None: ax.set_ylabel(y_label)
+
     ax.set_title(title)
-    ax.legend(loc="best", prop={"size": 10})
+    ax.legend(loc="best", prop={"size": 11})
 
     if filename:
         plt.savefig("%s.pdf" % filename)
@@ -109,13 +117,13 @@ def plot_bar(all_data, y_label=None, width=0.2, filename=None):
         y_mean = np.mean(data[0][1])
         rects = ax.bar([i*width], [y_mean], width=width, label=name, color=cmap_bar[key])
         rect = rects[0]
-        plt.text(rect.get_x() + rect.get_width() / 2.0, rect.get_height(), '%.2f' % y_mean, ha='center', va='bottom', fontsize=10)
+        plt.text(rect.get_x() + rect.get_width() / 2.0, rect.get_height(), '%.2f' % y_mean, ha='center', va='bottom', fontsize=9)
 
     if y_label is not None: ax.set_ylabel(y_label)
     ax.set_title(title)
     ax.set_xticks([])
     ax.set_xlim(-width, len(layout)*width-width)
-    ax.legend(loc="upper left", prop={"size": 10})
+    ax.legend(loc="best", prop={"size": 9})
 
     if filename:
         plt.savefig("%s.pdf" % filename)
@@ -127,54 +135,54 @@ def plot_bar(all_data, y_label=None, width=0.2, filename=None):
 
 if __name__ == "__main__":
     # Plot result on artifical data
-    # regression = ["make_friedman1", "make_friedman2", "make_friedman3"]
-    # classification = ["make_hastie_10_2", "make_waveforms", "make_twonorm", "make_threenorm", "make_ringnorm"]
+    regression = ["make_friedman1", "make_friedman2", "make_friedman3"]
+    classification = ["make_hastie_10_2", "make_waveforms", "make_twonorm", "make_threenorm", "make_ringnorm"]
 
-    # params = [("n_estimators", "params__n_estimators", True),
-    #           ("max_features", "params__max_features", False),
-    #           ("bootstrap", "params__bootstrap", False),
-    #           ("n_train", "stats__n_train", True),
-    #           ("n_features", "stats__n_features", True)]
+    params = [("n_estimators", "params__n_estimators", True),
+              ("max_features", "params__max_features", False),
+              ("bootstrap", "params__bootstrap", False),
+              ("n_train", "stats__n_train", True),
+              ("n_features", "stats__n_features", True)]
 
-    # stats = [("time_fit", "Fit time (s)"),
-    #          ("time_predict", "Predict time(s)"),
-    #          ("score_make_scorer(accuracy_score)", "Accuracy"),
-    #          ("score_make_scorer(roc_auc_score, needs_threshold=True)", "AUC"),
-    #          ("score_make_scorer(mean_squared_error, greater_is_better=False)", "MSE"),
-    #          ("score_make_scorer(r2_score)", "R2"),
-    #          ("leaves", "Leaves"),
-    #          ("average_depth", "Average depth")]
+    stats = [("time_fit", "Fit time (s)"),
+             ("time_predict", "Predict time(s)"),
+             ("score_make_scorer(accuracy_score)", "Accuracy"),
+             ("score_make_scorer(roc_auc_score, needs_threshold=True)", "AUC"),
+             ("score_make_scorer(mean_squared_error, greater_is_better=False)", "MSE"),
+             ("score_make_scorer(r2_score)", "R2"),
+             ("leaves", "Leaves"),
+             ("average_depth", "Average depth")]
 
-    # for dataset in regression+classification:
-    #     for prefix, param_field, curve in params:
-    #         files = [f for f in glob.glob("output/%s_*_%s*" % (prefix, dataset))]
+    for dataset in ["make_friedman1"]: #regression+classification:
+        for prefix, param_field, curve in params:
+            files = [f for f in glob.glob("output/%s_*_%s*" % (prefix, dataset))]
 
-    #         if len(files) == 0:
-    #             continue
+            if len(files) == 0:
+                continue
 
-    #         for stat_field, label in stats:
-    #             print dataset, prefix, stat_field
+            for stat_field, label in stats:
+                print dataset, prefix, stat_field
 
-    #             try:
-    #                 plot_curve(groupby(files, ["estimator", "generator"], param_field, "stats__%s" % stat_field),
-    #                            x_label=prefix,
-    #                            y_label=label,
-    #                            filename="figs/generators/%s/%s_%s" % (dataset, prefix, stat_field),
-    #                            curve=curve)
-    #             except:
-    #                 print "Failed!"
+                try:
+                    plot_curve(groupby(files, ["estimator", "generator"], param_field, "stats__%s" % stat_field),
+                               x_label=prefix,
+                               y_label=label,
+                               filename="figs/generators/%s/%s_%s" % (dataset, prefix, stat_field),
+                               curve=curve)
+                except:
+                    print "Failed!"
 
-    # Plot results on datasets
-    datasets = ["diabetes.npz", "dig44.npz", "ionosphere.npz", "pendigits.npz",
-                "letter.npz", "liver.npz", "musk2.npz", "ring-norm.npz", "satellite.npz",
-                "segment.npz", "sonar.npz", "spambase.npz", "two-norm.npz", "vehicle.npz",
-                "vowel.npz", "waveform.npz", "cifar10.npz", "mnist3vs8.npz", "mnist4vs9.npz", "mnist.npz",
-                "isolet.npz", "arcene.npz", "breast2.npz", "madelon.npz", "marti0.npz",
-                "reged0.npz", "secom.npz", "tis.npz", "sido0.npz"]
+    # # Plot results on datasets
+    # datasets = ["diabetes.npz", "dig44.npz", "ionosphere.npz", "pendigits.npz",
+    #             "letter.npz", "liver.npz", "musk2.npz", "ring-norm.npz", "satellite.npz",
+    #             "segment.npz", "sonar.npz", "spambase.npz", "two-norm.npz", "vehicle.npz",
+    #             "vowel.npz", "waveform.npz", "cifar10.npz", "mnist3vs8.npz", "mnist4vs9.npz", "mnist.npz",
+    #             "isolet.npz", "arcene.npz", "breast2.npz", "madelon.npz", "marti0.npz",
+    #             "reged0.npz", "secom.npz", "tis.npz", "sido0.npz"]
 
-    for dataset in datasets:
-        print dataset
-        files = glob.glob("output/default_*_%s*" % dataset)
-        plot_bar(groupby(files, ["estimator", "generator"], "estimator", "stats__time_fit"), y_label="Fit time (s)", filename="figs/datasets/%s_fit" % dataset)
-        plot_bar(groupby(files, ["estimator", "generator"], "estimator", "stats__time_predict"), y_label="Predict time (s)", filename="figs/datasets/%s_predict" % dataset)
-        plot_bar(groupby(files, ["estimator", "generator"], "estimator", "stats__score_make_scorer(accuracy_score)"), y_label="Accuracy", filename="figs/datasets/%s_accuracy" % dataset)
+    # for dataset in datasets:
+    #     print dataset
+    #     files = glob.glob("output/default_*_%s*" % dataset)
+    #     plot_bar(groupby(files, ["estimator", "generator"], "estimator", "stats__time_fit"), y_label="Fit time (s)", filename="figs/datasets/%s_fit" % dataset)
+    #     plot_bar(groupby(files, ["estimator", "generator"], "estimator", "stats__time_predict"), y_label="Predict time (s)", filename="figs/datasets/%s_predict" % dataset)
+    #     plot_bar(groupby(files, ["estimator", "generator"], "estimator", "stats__score_make_scorer(accuracy_score)"), y_label="Accuracy", filename="figs/datasets/%s_accuracy" % dataset)
